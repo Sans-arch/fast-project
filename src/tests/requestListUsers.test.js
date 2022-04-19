bfd8f7bd47d3951dbdcb9f3aa224af9e98331cd9
Santiago Fleitas <sansfleitas@gmail.com>
2022-04-19 20:00:46 -0300

    Modifying the async functions

--- src/tests/requestListUsers.test.js
+++ src/tests/requestListUsers.test.js
@@ -2,9 +2,7 @@ const request = require('supertest');
 const app = require('../../index');
 
 describe('Testando app server', () => {
-  it('return the list of users', async () => {
-    const res = await request(app).get('/users');
-
-    expect(res.body).toEqual(200);
+  it('return the list of users', () => {
+    request(app).get('/users').then(res => expect(res.status).toEqual(200))
   });
 });
